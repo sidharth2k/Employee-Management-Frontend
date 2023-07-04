a08221166dd135f96536c9733a39531d536fa7e5
Saad1929 <saad2baig123@gmail.com>
2023-07-04 22:37:10 +0100

    Add User Page Updated

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 export default function AddUser() {
-  return <div className="container">
-    <div className="row">
-      <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+          <div className="mb-3">
+            <label htmlFor="Name" className="form-label">
+              Name
+            </label>
+          </div>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }

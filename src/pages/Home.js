66c2af2c2c36810174d4373db9fa24297c88c815
Saad1929 <saad2baig123@gmail.com>
2023-07-04 22:00:18 +0100

    Backend Config Settings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
 
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:8080/users");
-    console.log(result.data);
+    setUsers(result.data);
   };
 
   return (

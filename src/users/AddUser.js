9eb11a42d3a810a9cdec70d4cefbb459eff9c134
Saad1929 <saad2baig123@gmail.com>
2023-07-04 22:43:28 +0100

    Cancel Button AddUser Page

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -41,7 +41,12 @@ export default function AddUser() {
               name="email"
             ></input>
           </div>
-          <button type="submit" className="btn btn-outline-primary">Submit</button>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
+          <button type="submit" className="btn btn-outline-danger mx-2">
+            Cancel
+          </button>
         </div>
       </div>
     </div>

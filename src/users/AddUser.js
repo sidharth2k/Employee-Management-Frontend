be2e4fd897646c257193066749f3826cb0bd3aa9
Saad1929 <saad2baig123@gmail.com>
2023-07-04 22:33:13 +0100

    Shadow for Add User Page

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AddUser() {
   return <div className="container">
     <div className="row">
-      <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">
+      <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
 
       </div>
     </div>

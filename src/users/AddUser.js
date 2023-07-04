2f193b866972acbc61fb620fd28e0d7a32bcfe8c
Saad1929 <saad2baig123@gmail.com>
2023-07-04 23:08:30 +0100

    Cancel Button Working

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AddUser() {
 
@@ -75,9 +75,9 @@ export default function AddUser() {
             <button type="submit" className="btn btn-outline-primary">
               Submit
             </button>
-            <button type="submit" className="btn btn-outline-danger mx-2">
+            <Link type="submit" className="btn btn-outline-danger mx-2" to="/">
               Cancel
-            </button>
+            </Link>
           </form>
         </div>
       </div>

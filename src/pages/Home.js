84306d43e328508a327c73aab0015cf982f5e933
Saad1929 <saad2baig123@gmail.com>
2023-07-04 21:45:23 +0100

    User Effect Function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,13 @@ export default function Home() {
   const[users,setUsers] = useState([])
   
   useEffect(()=>{
-    
-  })
+    loadUsers();
+  }, []);
 
   const loadUsers=async()=> {
     const result = await axios.get("http://localhost:8080/users");
+    console.log(result);
+
   }
 
   return (

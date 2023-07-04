718047d5c77ad840ee31d20c34b237a672742f94
Saad1929 <saad2baig123@gmail.com>
2023-07-04 22:32:49 +0100

    Add User Started

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function AddUser() {
-  return <div>AddUser From</div>;
+  return <div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">
+
+      </div>
+    </div>
+  </div>;
 }

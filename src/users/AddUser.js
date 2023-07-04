895ed8702ac28ce1043e13f09e5d7a5314261b67
Saad1929 <saad2baig123@gmail.com>
2023-07-04 23:03:04 +0100

    Deadlock Protection Form

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,22 +1,23 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function AddUser() {
+  const [user, setUsers] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
 
-  const [user,setUsers]=useState({
-    name:"",
-    username:"",
-    email:""
-  })
+  const { name, username, email } = user;
 
-  const{name,username,email}=user
-
-  const onInputChange=(e)=> {
-    setUsers({...user,[e.target.name]:e.target.value});
+  const onInputChange = (e) => {
+    setUsers({ ...user, [e.target.name]: e.target.value });
   };
 
-  const onSubmit=(e)=>{
+  const onSubmit = async (e) => {
     e.preventDefault();
-  }
+    await axios.post;
+  };
 
   return (
     <div className="container">
@@ -24,54 +25,54 @@ export default function AddUser() {
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Register User</h2>
 
-          <form onSubmit={(e)=>onSubmit(e)}>
-          <div className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Name"
-              name="name"
-              value={name}
-              onChange={(e)=>onInputChange(e)}
-            ></input>
-          </div>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              ></input>
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="Username" className="form-label">
-              Username
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Username"
-              name="username"
-              value={username}
-              onChange={(e)=>onInputChange(e)}
-            ></input>
-          </div>
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              ></input>
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="Email" className="form-label">
-              E-mail
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="E-mail"
-              name="email"
-              value={email}
-              onChange={(e)=>onInputChange(e)}
-            ></input>
-          </div>
-          <button type="submit" className="btn btn-outline-primary">
-            Submit
-          </button>
-          <button type="submit" className="btn btn-outline-danger mx-2">
-            Cancel
-          </button>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="E-mail"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              ></input>
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <button type="submit" className="btn btn-outline-danger mx-2">
+              Cancel
+            </button>
           </form>
         </div>
       </div>

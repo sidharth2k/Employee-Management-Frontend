c70b6361f88bf3a0ae515f9667e807a0a430707e
Saad1929 <saad2baig123@gmail.com>
2023-07-05 21:53:13 +0100

    Home Page Link

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -6,9 +6,9 @@ export default function navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to="/">
             Employee Management System
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"

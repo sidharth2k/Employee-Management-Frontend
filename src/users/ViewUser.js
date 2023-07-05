417a7c2930cfef1b6c3fe7e33a4bfed4059d5466
Saad1929 <saad2baig123@gmail.com>
2023-07-05 21:23:04 +0100

    Frontend View User Page Started

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -1,1 +1,13 @@
-// test
+import React from "react";
+
+export default function ViewUser() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+        </div>
+      </div>
+    </div>
+  );
+}

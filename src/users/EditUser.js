3f2170226ec588925d61ccbb9781d984861e7e86
Saad1929 <saad2baig123@gmail.com>
2023-07-05 21:10:22 +0100

    Edit Page Changes

--- src/users/EditUser.js
+++ src/users/EditUser.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function EditUser() {
+  let navigate = useNavigate();
 
-  let navigate=useNavigate();
-
-  const {id} = useParams();
+  const { id } = useParams();
 
   const [user, setUsers] = useState({
     name: "",
@@ -20,13 +19,20 @@ export default function EditUser() {
     setUsers({ ...user, [e.target.name]: e.target.value });
   };
 
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   const onSubmit = async (e) => {
     e.preventDefault();
     await axios.put(`http://localhost:8080/user/${id}`, user);
     navigate("/");
   };
 
-  
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    setUsers(result.data);
+  };
 
   return (
     <div className="container">

148b2af95c27ab68c29d29f4558cb1ca04ee0ffb
Saad1929 <saad2baig123@gmail.com>
2023-07-05 21:25:37 +0100

    User Card

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -5,7 +5,19 @@ export default function ViewUser() {
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-          <h2 className="text-center m-4">Register User</h2>
+          <h2 className="text-center m-4">User Details</h2>
+
+          <div className="card">
+            <div className="card-header">
+                Details of User ID: 
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <b>Name:</b>
+                        
+                    </li>
+                </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>

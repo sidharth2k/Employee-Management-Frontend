2ce2f5cd20bffb611ba03f4681585065ecb0861b
sidharth2k <sidsidharthan2000@gmail.com>
2025-02-08 04:56:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/adduser" element={<AddUser />} />
